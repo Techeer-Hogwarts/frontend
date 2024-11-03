2c4bd2525ffbe4a2d48ee0de5de7b0531232ec5e
백지윤 <jy.baek079@gmail.com>
2024-11-03 19:14:27 +0900

    fix: 통합검색 블로그, 세션 카드 컴포넌트 분리

--- src/components/search/BlogSection.tsx
+++ src/components/search/BlogSection.tsx
@@ -1,32 +1,20 @@
-import Image from 'next/image'
 import Section from './Section'
+import CardItem from './CardItem'
 
 const BlogSection = () => {
   return (
     <Section id="blog" title="블로그">
       <div className="grid grid-cols-4 gap-[3rem]">
         {[...Array(8)].map((_, index) => (
-          <div key={index} className="w-[16.125rem]">
-            <div className="mb-3">
-              <Image
-                src="/images/session/thumbnail.png"
-                alt="thumbnail"
-                width={258}
-                height={136}
-              />
-            </div>
-            <h4 className="text-xl font-semibold mb-2">
-              프론트엔드 버추얼리제션
-            </h4>
-            <p className="text-[10px] text-gray mb-2">2024년 여름</p>
-            <span className="text-xs text-pink bg-lightprimary py-1 px-2 rounded-md">
-              Frontend
-            </span>
-            <div className="flex items-center mt-2">
-              <Image src="/profile.png" alt="profile" width={20} height={20} />
-              <span className="font-semibold text-xs ml-1">주영준</span>
-            </div>
-          </div>
+          <CardItem
+            key={index}
+            title="프론트엔드 시각화"
+            date="2024년 여름"
+            category="Frontend"
+            author="미영"
+            authorImage="/profile.png"
+            thumbnail="/images/session/thumbnail.png"
+          />
         ))}
       </div>
     </Section>
--- src/components/search/SessionSection.tsx
+++ src/components/search/SessionSection.tsx
@@ -1,32 +1,20 @@
-import Image from 'next/image'
 import Section from './Section'
+import CardItem from './CardItem'
 
 const SessionSection = () => {
   return (
     <Section id="session" title="세션">
       <div className="grid grid-cols-4 gap-[3rem]">
         {[...Array(8)].map((_, index) => (
-          <div key={index} className="w-[16.125rem]">
-            <div className="mb-3">
-              <Image
-                src="/images/session/thumbnail.png"
-                alt="thumbnail"
-                width={258}
-                height={136}
-              />
-            </div>
-            <h4 className="text-xl font-semibold mb-2">
-              프론트엔드 버추얼리제션
-            </h4>
-            <p className="text-[10px] text-gray mb-2">2024년 여름</p>
-            <span className="text-xs text-pink bg-lightprimary py-1 px-2 rounded-md">
-              Frontend
-            </span>
-            <div className="flex items-center mt-2">
-              <Image src="/profile.png" alt="profile" width={20} height={20} />
-              <span className="font-semibold text-xs ml-1">주영준</span>
-            </div>
-          </div>
+          <CardItem
+            key={index}
+            title="백엔드 기초"
+            date="2024년 가을"
+            category="Backend"
+            author="주영준"
+            authorImage="/profile.png"
+            thumbnail="/images/session/thumbnail.png"
+          />
         ))}
       </div>
     </Section>
