e0d15a1076f14214f6d2697fc9aca886bd489058
백지윤 <jy.baek079@gmail.com>
2024-11-03 18:56:32 +0900

    test: 스토리북 수정

--- src/stories/signup/ExperienceSection.stories.tsx
+++ src/stories/signup/ExperienceSection.stories.tsx
@@ -1,35 +1,39 @@
 import React, { useState } from 'react'
-import { Meta, StoryFn } from '@storybook/react'
-import ExperienceSection, {
-  ExperienceSectionProps,
-} from '@/components/signup/ExperienceSection'
+import { Meta, StoryObj } from '@storybook/react'
+import ExperienceSection from '@/components/signup/ExperienceSection'
 
-export default {
+const meta: Meta<typeof ExperienceSection> = {
   title: 'Signup/ExperienceSection',
   component: ExperienceSection,
-} as Meta
-
-const Template: StoryFn<ExperienceSectionProps> = (args) => {
-  const [experienceStatus, setExperienceStatus] = useState<string | null>(null)
-  const [items, setItems] = useState<number[]>([])
-
-  const addExperience = () => setItems([...items, items.length + 1])
-  const removeExperience = (index: number) =>
-    setItems(items.filter((_, i) => i !== index))
-
-  return (
-    <ExperienceSection
-      {...args}
-      experienceStatus={experienceStatus}
-      setExperienceStatus={setExperienceStatus}
-      items={items}
-      addExperience={addExperience}
-      removeExperience={removeExperience}
-    />
-  )
+  tags: ['autodocs'],
 }
 
-export const Default = Template.bind({})
-Default.args = {
-  title: '인턴 경험',
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: (args) => {
+    const [experienceStatus, setExperienceStatus] = useState<string | null>(
+      null,
+    )
+    const [items, setItems] = useState<number[]>([])
+
+    const addExperience = () => setItems([...items, items.length + 1])
+    const removeExperience = (index: number) =>
+      setItems(items.filter((_, i) => i !== index))
+
+    return (
+      <ExperienceSection
+        {...args}
+        experienceStatus={experienceStatus}
+        setExperienceStatus={setExperienceStatus}
+        items={items}
+        addExperience={addExperience}
+        removeExperience={removeExperience}
+      />
+    )
+  },
+  args: {
+    title: '인턴 경험',
+  },
 }
--- src/stories/signup/PositionSelect.stories.tsx
+++ src/stories/signup/PositionSelect.stories.tsx
@@ -1,35 +1,37 @@
 import React, { useState } from 'react'
-import { Meta, StoryFn } from '@storybook/react'
-import PositionSelect, {
-  PositionSelectProps,
-} from '@/components/signup/PositionSelect'
+import { Meta, StoryObj } from '@storybook/react'
+import PositionSelect from '@/components/signup/PositionSelect'
 
-export default {
+const meta: Meta<typeof PositionSelect> = {
   title: 'Signup/PositionSelect',
   component: PositionSelect,
-} as Meta
-
-const Template: StoryFn<PositionSelectProps> = (args) => {
-  const [selectedPositions, setSelectedPositions] = useState<string[]>([])
-
-  const handlePositionSelect = (position: string) => {
-    if (selectedPositions.includes(position)) {
-      setSelectedPositions(
-        selectedPositions.filter((item) => item !== position),
-      )
-    } else if (selectedPositions.length < 2) {
-      setSelectedPositions([...selectedPositions, position])
-    }
-  }
-
-  return (
-    <PositionSelect
-      {...args}
-      selectedPositions={selectedPositions}
-      handlePositionSelect={handlePositionSelect}
-    />
-  )
+  tags: ['autodocs'],
 }
 
-export const Default = Template.bind({})
-Default.args = {}
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: (args) => {
+    const [selectedPositions, setSelectedPositions] = useState<string[]>([])
+
+    const handlePositionSelect = (position: string) => {
+      if (selectedPositions.includes(position)) {
+        setSelectedPositions(
+          selectedPositions.filter((item) => item !== position),
+        )
+      } else if (selectedPositions.length < 2) {
+        setSelectedPositions([...selectedPositions, position])
+      }
+    }
+
+    return (
+      <PositionSelect
+        {...args}
+        selectedPositions={selectedPositions}
+        handlePositionSelect={handlePositionSelect}
+      />
+    )
+  },
+  args: {},
+}
