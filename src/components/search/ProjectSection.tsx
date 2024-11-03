c80cafdda02bb452b09163a14cc5dddb29831264
백지윤 <jy.baek079@gmail.com>
2024-11-04 00:37:30 +0900

    fix: key 값 변경

--- src/components/search/ProjectSection.tsx
+++ src/components/search/ProjectSection.tsx
@@ -2,11 +2,21 @@ import Card from '@/components/project/Card'
 import Section from './Section'
 
 const ProjectSection = () => {
+  const dummyData = [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+    { id: '4' },
+    { id: '5' },
+    { id: '6' },
+    { id: '7' },
+    { id: '8' },
+  ]
   return (
     <Section id="project" title="프로젝트">
       <div className="grid grid-cols-4 gap-5">
-        {[...Array(8)].map((_, index) => (
-          <Card key={index} />
+        {dummyData.map((item) => (
+          <Card key={item.id} />
         ))}
       </div>
     </Section>
