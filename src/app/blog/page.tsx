ab262f6fd037936e0f4e8d85042248f7c14a4c31
Yeongjun Joo <wndudwns6824@naver.com>
2024-12-21 23:23:58 +0900

    feat : 블로그 api 연동

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,47 +1,104 @@
+'use client'
 import TapBar from '@/components/common/TapBar'
 import AddBtn from '@/components/common/AddBtn'
 import BlogPost from '@/components/blog/BlogPost'
+import { useTapBarStore } from '@/store/tapBarStore'
+import { useEffect, useState } from 'react'
 
-let blog = [
-  {
-    title: '왜 웹소켓을 사용하는가',
-    name: '주영준',
-    date: '2024년 9월',
-  },
-  {
-    title: '왜 웹소켓을 사용하는가',
-    name: '우아한',
-    date: '2019년 11월',
-  },
-  {
-    title: '흑백요리사 심사위원으로 출현한 소감, 흑백요리사 시즌2에 대하여',
-    name: '백종원',
-    date: '2032년 1월',
-  },
-  {
-    title: '왜 웹소켓을 사용하는가',
-    name: '우아한',
-    date: '2019년 11월',
-  },
-]
+interface User {
+  name: string
+}
 
-export default function page() {
+interface BlogProps {
+  title: string
+  id: string
+  date: string
+  url: string
+  likeCount: number
+  user: User
+}
+
+export default function Page() {
+  const [blog, setBlog] = useState<BlogProps[]>([])
+  const [message, setMessage] = useState<string | null>(null)
+  const handleDeleteSession = (id: string) => {
+    setBlog((prevblogs) => prevblogs.filter((blog) => blog.id !== id))
+    setMessage('블로그 글이 삭제되었습니다.')
+    setTimeout(() => setMessage(null), 2000)
+  }
+  const getBestBlog = async () => {
+    try {
+      const response = await fetch(
+        'https://api.techeerzip.cloud/api/v1/blogs/best?offset=0&limit=10',
+        {
+          method: 'GET',
+        },
+      )
+
+      if (!response.ok) {
+        throw new Error('세션 데이터를 업로드하는 데 실패했습니다.')
+      }
+
+      const result = await response.json()
+      setBlog(result.data)
+      console.log(blog)
+      console.log('블로그api가 성공적으로 통신되었습니다:', result.data)
+    } catch (err) {
+      console.error('블로그 데이터 업로드 중 오류 발생:', err)
+    }
+  }
+  const getBlog = async () => {
+    const baseUrl = 'https://api.techeerzip.cloud/api/v1/blogs'
+    const params = {
+      keyword: '',
+      category: '',
+      position: '',
+      offset: String(0),
+      limit: String(10),
+    }
+
+    const filteredParams = Object.fromEntries(
+      Object.entries(params).filter(
+        ([_, value]) => value !== null && value !== '',
+      ),
+    )
+    const queryString = new URLSearchParams(filteredParams).toString()
+    const url = `${baseUrl}?${queryString}`
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setBlog(data.data || [])
+      })
+  }
+  const { activeOption } = useTapBarStore()
+  useEffect(() => {
+    if (activeOption == '금주의 블로그') {
+      getBestBlog()
+    } else if (activeOption == 'Techeer' || activeOption == 'Shared') {
+      getBlog()
+    }
+  }, [activeOption])
+
   return (
     <div className="flex justify-center h-auto min-h-screen">
       <div className="flex flex-col">
+        {message && (
+          <div className="bg-red-500/80 z-50 rounded-md fixed text-white text-center bottom-5 left-1/2 transform -translate-x-1/2 px-4 py-2">
+            {message}
+          </div>
+        )}
         <div className="w-[1200px] text-left mt-14 mb-[2.84rem]">
           <p className="text-[2.5rem] font-bold">블로그</p>
           <p className="text-[1.25rem]">테커인들의 블로그를 확인해보세요.</p>
         </div>
         <TapBar
-          options={[
-            '전체보기',
-            'Frontend',
-            'Bakcend',
-            'DevOps',
-            'Coding Test',
-            'Others',
-          ]}
+          options={['금주의 블로그', 'Techeer', 'Shared']}
           placeholder="블로그 제목 혹은 이름을 검색해보세요"
         />
         <div className="grid grid-cols-3 gap-8 mt-8">
@@ -49,8 +106,12 @@ export default function page() {
             <BlogPost
               key={index}
               title={blog.title}
+              id={blog.id}
               date={blog.date}
-              name={blog.name}
+              url={blog.url}
+              likeCount={blog.likeCount}
+              name={blog.user.name}
+              onDelete={handleDeleteSession}
             />
           ))}
         </div>
