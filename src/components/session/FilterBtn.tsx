df80131881b097a458d1ef073f033ffad02037c8
Yeongjun Joo <wndudwns6824@naver.com>
2025-02-10 00:25:58 +0900

    feat : 필터버튼 삭제 구현

--- src/components/session/FilterBtn.tsx
+++ src/components/session/FilterBtn.tsx
@@ -1,10 +1,16 @@
+'use client'
 import Image from 'next/image'
+import { Dispatch, SetStateAction } from 'react'
 
 export interface FilterBtnProps {
   title: string
+  setSelectedPeriods: Dispatch<SetStateAction<string[]>>
 }
 
-export default function FilterBtn({ title }: FilterBtnProps) {
+export default function FilterBtn({
+  title,
+  setSelectedPeriods,
+}: FilterBtnProps) {
   return (
     <div className="relative w-[167px] flex items-center justify-center rounded-2xl text-lg h-9 border bg-[#FFF6F0] text-[#DD7E3A] border-primary">
       {title}
@@ -14,6 +20,7 @@ export default function FilterBtn({ title }: FilterBtnProps) {
         width={9}
         height={9}
         className="absolute right-2"
+        onClick={() => setSelectedPeriods([])}
       />
     </div>
   )
