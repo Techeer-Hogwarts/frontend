8b2f5df9d496245c4a6c57b4db5cb3585204a21a
백지윤 <jy.baek079@gmail.com>
2025-02-17 19:46:51 +0900

    fix: 프로젝트, 스터디 더미 삭제, 이미지 오류 시 기본 이미지 대체

--- src/components/mypage/Carousel.tsx
+++ src/components/mypage/Carousel.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Slider from 'react-slick'
 import Image from 'next/image'
 import { HiMiniChevronLeft, HiMiniChevronRight } from 'react-icons/hi2'
+import { FaRegImage } from 'react-icons/fa6'
 import Link from 'next/link'
 
 interface Team {
   id: number
   name: string
-  mainImage: string
+  resultImages: string[]
 }
 
 interface CarouselProps {
@@ -38,6 +39,32 @@ function PrevArrow(props: any) {
   )
 }
 
+// 이미지 혹은 대체 아이콘을 렌더링하는 컴포넌트
+function ImageOrIcon({ src, alt }: { src?: string; alt: string }) {
+  const [imgError, setImgError] = useState(false)
+
+  // src가 없거나 빈 문자열, 혹은 onError 발생 시 => 아이콘 표시
+  if (!src || imgError) {
+    return (
+      <div className="w-[64px] h-[64px] flex items-center justify-center bg-lightgray text-gray rounded">
+        <FaRegImage size={32} />
+      </div>
+    )
+  }
+
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={64}
+      height={64}
+      className="rounded"
+      unoptimized
+      onError={() => setImgError(true)}
+    />
+  )
+}
+
 const Carousel: React.FC<CarouselProps> = ({ teams, routePrefix }) => {
   const settings = {
     dots: false,
@@ -59,13 +86,7 @@ const Carousel: React.FC<CarouselProps> = ({ teams, routePrefix }) => {
             href={`${routePrefix}/${team.id}`}
             className="relative group cursor-pointer"
           >
-            <Image
-              src={team.mainImage}
-              alt={team.name}
-              width={64}
-              height={64}
-              className="rounded"
-            />
+            <ImageOrIcon src={team.resultImages?.[0]} alt={team.name} />
             <div className="absolute w-[64px] h-[64px] top-0 bg-black bg-opacity-60 text-white text-xs flex items-center justify-center text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded">
               {team.name}
             </div>
@@ -87,13 +108,7 @@ const Carousel: React.FC<CarouselProps> = ({ teams, routePrefix }) => {
             href={`${routePrefix}/${team.id}`}
             className="relative group cursor-pointer"
           >
-            <Image
-              src={team.mainImage}
-              alt={team.name}
-              width={64}
-              height={64}
-              className="rounded"
-            />
+            <ImageOrIcon src={team.resultImages?.[0]} alt={team.name} />
             <div className="absolute w-[64px] h-[64px] top-0 bg-black bg-opacity-60 text-white text-xs flex items-center justify-center text-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded">
               {team.name}
             </div>
