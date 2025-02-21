78638833720c81f6fffb2e6572ae3e6fe5e92699
Yeongjun Joo <wndudwns6824@naver.com>
2025-02-21 21:02:11 +0900

    fix : 세션 삭제 오류 수정

--- src/app/session/_lib/deleteSession.ts
+++ src/app/session/_lib/deleteSession.ts
@@ -1,22 +1,31 @@
 export const deleteSession = async (id: string) => {
-  const response = await fetch(
-    `https://api.techeerzip.cloud/api/v1/sessions/${id}`,
-    {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
+  const response = await fetch(`/api/v1/sessions/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
-  if (response.status == 403) {
+    credentials: 'include',
+  })
+
+  if (response.status === 403) {
     alert('본인이 작성한 게시물만 삭제할 수 있습니다.')
     throw new Error('세션 데이터를 삭제하는데 실패했습니다.')
   }
+
   if (!response.ok) {
+    const errorDetails = await response.text()
+    console.error('서버 오류 응답:', errorDetails)
     throw new Error('세션 데이터를 삭제하는데 실패했습니다.')
   }
 
-  const result = await response.json()
-  return result.data
+  if (response.status === 204) {
+    return {}
+  }
+  try {
+    const result = await response.json()
+    return result
+  } catch (error) {
+    console.error('응답 본문 JSON 파싱 오류:', error)
+    return {} // JSON 파싱 오류 발생 시 빈 객체 반환
+  }
 }
