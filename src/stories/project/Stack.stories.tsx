f79e8f3d8eabd1499e3341475c9a22c888af2315
백지윤 <jy.baek079@gmail.com>
2025-02-23 04:46:21 +0900

    fix

--- src/stories/project/Stack.stories.tsx
+++ src/stories/project/Stack.stories.tsx
@@ -1,63 +1,63 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Stack from '@/components/project/detail/Stack'
+// import { Meta, StoryObj } from '@storybook/react'
+// import Stack from '@/components/project/detail/Stack'
 
-const meta: Meta<typeof Stack> = {
-  component: Stack,
-  title: 'Components/Stack',
-  tags: ['autodocs'],
-}
+// const meta: Meta<typeof Stack> = {
+//   component: Stack,
+//   title: 'Components/Stack',
+//   tags: ['autodocs'],
+// }
 
-export default meta
+// export default meta
 
-type Story = StoryObj<typeof Stack>
+// type Story = StoryObj<typeof Stack>
 
-// 기본 스토리
-export const Default: Story = {
-  args: {
-    stacks: {
-      backendStack: ['Go Lang', 'PostgreSQL', 'Nest.JS', 'Python'],
-      frontendStack: ['Zustand', 'Next.js', 'TS'],
-      devopsStack: ['Jenkins CI', 'GCP', 'Terraform', 'Github Actions'],
-    },
-  },
-}
+// // 기본 스토리
+// export const Default: Story = {
+//   args: {
+//     stacks: {
+//       backendStack: ['Go Lang', 'PostgreSQL', 'Nest.JS', 'Python'],
+//       frontendStack: ['Zustand', 'Next.js', 'TS'],
+//       devopsStack: ['Jenkins CI', 'GCP', 'Terraform', 'Github Actions'],
+//     },
+//   },
+// }
 
-// 스택이 많을 때
-export const ManyStacks: Story = {
-  args: {
-    stacks: {
-      backendStack: [
-        'Go Lang',
-        'PostgreSQL',
-        'Nest.JS',
-        'Python',
-        'Node.js',
-        'Ruby',
-        'Java',
-        'Spring Boot',
-      ],
-      frontendStack: [
-        'Zustand',
-        'Next.js',
-        'TS',
-        'React',
-        'Redux',
-        'Vue.js',
-        'JavaScript',
-        'TypeScript',
-        'TypeScript',
-        'TypeScript',
-      ],
-      devopsStack: [
-        'Jenkins CI',
-        'GCP',
-        'Terraform',
-        'Github Actions',
-        'AWS',
-        'Kubernetes',
-        'Docker',
-        'Azure',
-      ],
-    },
-  },
-}
+// // 스택이 많을 때
+// export const ManyStacks: Story = {
+//   args: {
+//     stacks: {
+//       backendStack: [
+//         'Go Lang',
+//         'PostgreSQL',
+//         'Nest.JS',
+//         'Python',
+//         'Node.js',
+//         'Ruby',
+//         'Java',
+//         'Spring Boot',
+//       ],
+//       frontendStack: [
+//         'Zustand',
+//         'Next.js',
+//         'TS',
+//         'React',
+//         'Redux',
+//         'Vue.js',
+//         'JavaScript',
+//         'TypeScript',
+//         'TypeScript',
+//         'TypeScript',
+//       ],
+//       devopsStack: [
+//         'Jenkins CI',
+//         'GCP',
+//         'Terraform',
+//         'Github Actions',
+//         'AWS',
+//         'Kubernetes',
+//         'Docker',
+//         'Azure',
+//       ],
+//     },
+//   },
+// }
