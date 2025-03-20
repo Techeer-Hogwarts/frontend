b28f0dbf436824637ac0eb9758306939d7a32242
Yeongjun Joo <wndudwns6824@naver.com>
2025-03-20 13:05:04 +0900

    refactor : 탭바 수정

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -10,6 +10,7 @@ import FilterBtn from '@/components/session/FilterBtn'
 import ResumeList from './@resumeList'
 import SearchBar from '@/components/common/SearchBar'
 import AuthModal from '@/components/common/AuthModal'
+import { useTapBarStore } from '@/store/tapBarStore'
 
 export default function Resume() {
   const router = useRouter() // Resume 페이지에서 useRouter 사용
@@ -44,11 +45,7 @@ export default function Resume() {
       setSelectedYear(selectedYear.filter((item) => item !== filter))
     }
   }
-
-  // 카테고리 변경
-  const handleCategoryChange = (category: string) => {
-    setSelectedCategory(category)
-  }
+  const { activeOption } = useTapBarStore()
 
   // // 검색어 저장 및 이력서 목록 업데이트
   // const handleSearch = (query: string) => {
@@ -90,7 +87,7 @@ export default function Resume() {
       <div className="flex flex-col">
         <div className="flex justify-between">
           {/** 기수 탭 */}
-          <TapBar options={category} onSelect={handleCategoryChange} />
+          <TapBar options={category} />
           {/** 검색창 */}
           <SearchBar
             placeholder="이름 또는 키워드로 검색해보세요"
@@ -144,7 +141,7 @@ export default function Resume() {
         <ResumeList
           position={selectedPosition}
           year={selectedYear}
-          category={selectedCategory}
+          category={activeOption}
         />
       </div>
     </div>
