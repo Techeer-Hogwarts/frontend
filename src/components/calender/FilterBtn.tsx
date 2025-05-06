b4f87abcc9be2ce531a6c03e1e74b0b8fcd9bf51
choi-il-woo <chldlfdn0440@naver.com>
2025-05-06 14:41:48 +0900

    design: 버튼 전환 부드럽게 수정

--- src/components/calender/FilterBtn.tsx
+++ src/components/calender/FilterBtn.tsx
@@ -5,13 +5,24 @@ interface FilterBtnProps {
   isClicked?: boolean
   onClick: () => void
 }
-export default function FilterBtn({ title, isClicked, onClick }: FilterBtnProps) {
+
+export default function FilterBtn({
+  title,
+  isClicked,
+  onClick,
+}: FilterBtnProps) {
   return (
     <button
       type="button"
-      className={`h-8 px-6 border rounded-2xl text-lg ${!isClicked ? 'text-gray bg-lightgray/50 border-gray' : 'text-[#DD7E3A] bg-[#FFF6F0] border-primary'}`}
       onClick={onClick}
->
+      className={`h-8 px-6 border rounded-2xl text-lg transition-colors duration-200
+        ${
+          !isClicked
+            ? 'text-gray bg-lightgray/50 border-gray'
+            : 'text-[#DD7E3A] bg-[#FFF6F0] border-primary'
+        }
+      `}
+    >
       {title}
     </button>
   )
